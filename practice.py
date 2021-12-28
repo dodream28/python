85fab986e1e5e9dcb870fd836751d73a417aab7f
dg oh <dodream288@gmail.com>
2021-12-28 16:18:25 +0900

    연산자

--- practice.py
+++ practice.py
@@ -1,4 +1,18 @@
-print(5)
-print(-10)
-print(3.14)
-print(1000)
+print(1+1)
+print(3-2)
+print(5*2)
+print(6/3)
+
+print(2**3) # 2^3 = 8
+print(5%3) # 나머지
+print(10%3)
+print(5//3)
+print(10//3) # 몫
+
+print(10 > 3) # Ture
+print(4 >= 7) # False
+print(5 <= 5) # Ture
+
+print(3 == 3)
+print(4 == 2)
+print(3+4 == 7)
