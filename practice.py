3786c7b1644fe9bc3894d3de8b39255ab885a549
dg oh <dodream288@gmail.com>
2021-12-29 11:14:38 +0900

    Quiz

--- practice.py
+++ practice.py
@@ -1,15 +1,42 @@
-menu = ("돈까스", "치즈까스")
-print(menu[0])
-print(menu[1])
+"""
+Quiz) 당신의 학교에서는 파이썬 코딩 대회를 주최합니다.
+참석률을 높이기 위해 댓글 이벤트를 진행하기로 하였습니다.
+댓글 작성자들 중에 추첨을 통해 1명은 치킨, 3명은 커피 쿠폰을 받게 됩니다.
+추첨 프로그램을 작성하시오.
 
-# menu.add("생선까스")
+조건1 : 편의상 댓글은 20명이 작성하였고 아이디는 1~20 이라고 가정
+조건2 : 댓글 내용과 상관 없이 무작위로 추첨하되 중복 불가
+조건3 : random 모듈의 shuffle 과 sample을 활용
 
-# name = "김종국"
-# age = 20
-# hobby = "코딩"
-# print(name, age, hobby)
+(출력 예제)
+ -- 당첨자 발표 --
+ 치킨 당첨자 : 1
+ 커피 당첨자 : [2, 3, 4]
+  -- 축하합니다 --
 
-(name, age, hobby) = ("김종국", 20, "코딩")
-# name, age, hobby = "김종국", 20, "코딩"
+(활용 예제)
+from random import *
+lst = [1, 2, 3, 4, 5]
+print(lst)
+shuffle(lst)
+print(lst)
 
-print(name, age, hobby)
+#print(sample(lst, 1))
+"""
+from random import *
+
+# Id = [randint(1, 20)]
+# print(Id)
+
+
+user = range(1, 21)
+user = list(user)
+
+winners = sample(user, 4) # 4명 중에서 1명은 치킨, 3명은 커피
+# print(winners)
+# print(user)
+
+print( " -- 당첨자 발표 -- ")
+print("치킨 당첨자 : {0}".format(winners[0]))
+print("커피 당첨자 : {0}".format(winners[1:]))
+print(" -- 축하합니다 -- ")
