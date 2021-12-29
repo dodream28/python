59710fe8eb7f48f1ff2443e6c0651d4f93eb8a59
dg oh <dodream288@gmail.com>
2021-12-29 09:34:24 +0900

    list

--- practice.py
+++ practice.py
@@ -1,27 +1,65 @@
-"""
-Quiz ) 사이트별로 비밀번호를 만들어 주는 프로그램을 작성하시오
+# 리스트 []
 
-예) http://naver.com
-규칙1 : http:// 부분은 제외 => naver.com
-규칙2 : 처음 만나는 점(.) 이후 부분은 제외 => naver
-규칙3 : 남은 글자 중 처음 세자리 + 글자 갯수 + 글자 내 'e' 갯수 + "!"로 구성
+# 지하철 칸별로 10명, 20명, 30명
 
-                (nav)               (5)             (1)         (!)   
+# subway1 = 10
+# subway2 = 20
+# subway3 = 30
 
-예) 생성된 비밀번호 : nav51!
 
-"""
+subway = [10, 20, 30]
+print(subway)
 
-url = "http://naver.com"
 
-my_str = url.replace("http://", "")
+subway = ["유재석", "조세호", "박명수"]
+print(subway)
 
-my_str = my_str[:my_str.index(".")]
+# 조세호씨가 몇 번째 칸에 타고 있는가?
+print(subway.index("조세호"))
 
-len = len(my_str)
-lang = my_str[0:3]
-enumber = my_str.count("e")
+# 하하씨가 다음 정류장에서 다음 칸에 탐
 
-password = str(lang) + str(len) + str(enumber) + "!"
+subway.append("하하")
+print(subway)
 
-print("{} 의 비밀번호는 {} 입니다.".format(url, password))
+# 정형돈씨를 유재석 / 조세호 사이에 태워봄
+subway.insert(1, "정형돈")
+print(subway)
+
+# 지하철에 있는 사람을 한 명씩 뒤에서 꺼냄
+#print(subway.pop())
+#print(subway)
+
+#print(subway.pop())
+#print(subway)
+
+#print(subway.pop())
+#print(subway)
+
+# 같은 이름의 사람이 몇 명 있는지 확인
+subway.append("유재석")
+print(subway)
+print(subway.count("유재석"))
+
+# 정렬도 가능
+num_list = [5,2,4,3,1]
+num_list.sort()
+print(num_list)
+
+# 순서 뒤집기 가능
+num_list.reverse()
+print(num_list)
+
+# 모두 지우기
+
+num_list.clear()
+print(num_list)
+
+# 다양한 자료형 함께 사용
+mix_list = ["조세호", 20, True]
+int_list = [5, 2, 4, 3, 1]
+#print(mix_list)
+
+# 리스트 확장
+int_list.extend(mix_list)
+print(int_list)
