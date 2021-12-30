8fe31f54cdfc9d5711cbfda95cd0927242766abc
dg oh <dodream288@gmail.com>
2021-12-30 17:00:13 +0900

    Quiz#6 해답

--- practice.py
+++ practice.py
@@ -29,18 +29,12 @@ Quiz ) 표준 체중을 구하는 프로그램을 작성하시오
 # std_weight(172, "남자")
 
 def std_weight(height, gender) :
-    heightcm = height*0.01
     if gender == "여자" :
-        weight=heightcm*heightcm*22
-        return 
+        return height*height*22
     if gender == "남자" : 
-        weight=heightcm*heightcm*21
-        return 
+        return height*height*21
 height = 183
 gender = "남자"
-weight = 0
+weight = round(std_weight(height / 100, gender), 2)
 
-print("{} {} {}".format(height, gender, weight))
-
-
- print("키 {}cm {}의 표준 체중은 {:2.f}kg 입니다.".format(height, gender, weight))
+print("키 {}cm {}의 표준 체중은 {}kg 입니다.".format(height, gender, weight))
