3ef5921a51d38dcff29dd8bdd8c53921bd1f188e
dg oh <dodream288@gmail.com>
2021-12-30 15:16:34 +0900

    함수기본값

--- practice.py
+++ practice.py
@@ -1,25 +1,22 @@
-def open_account():
-    print("새로운 계좌가 생성되었습니다.")
+# def profile(name, gender, department):
+#     print("이름 : {}\t성별 : {}\t부서 : {} ".format(name, gender, department))
+    
 
-def deposit(balance, money):
-    print("입금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance + money))
-    return balance + money
+# profile("오동규", "남", "MSP")
+# profile("서동아", "여", "CC1Group")
 
-balance = 0 # 잔액
-balance = deposit(balance, 1000)
-print(balance)
+# def profile(name, age, main_lang):
+#     print("이름 : {}\t나이 : {}\t주 사용 언어: {}".format(name, age, main_lang))
 
-def withdraw(balance, money): #출금
-    if balance >- money: # 잔액이 출금보다 많으면
-        print("출금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance - money))
-        return balance - money
-    else :
-        print("출금이 완료되지 않았습니다. 잔액은 {0} 원입니다.".format(balance))
-        return balance
+# profile("유재석", 20, "파이썬")
+# profile("김태호", 25, "자바")
 
-def withdraw_night(balance, money): #저녁에 출금
-    commission = 100 # 수수료 100원
-    return commission, balance - money - commission
 
-commission, balance = withdraw_night(balance, 500)
-print("수수료 {0} 원이며, 잔액은 {1} 원입니다.".format(commission, balance))
+### 같은 학교 같은 학년 같은 반 같은 수업.
+
+
+def profile(name, age=17, main_lang="파이썬"):
+    print("이름 : {}\t나이 : {}\t주 사용 언어: {}".format(name, age, main_lang))
+
+profile("유재석")
+profile("김태호")
