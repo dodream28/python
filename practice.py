e96489db545fa7d87df0c5b0dc681a9c01291cef
dg oh <dodream288@gmail.com>
2021-12-30 15:56:19 +0900

    가변인자

--- practice.py
+++ practice.py
@@ -1,22 +1,13 @@
-# def profile(name, gender, department):
-#     print("이름 : {}\t성별 : {}\t부서 : {} ".format(name, gender, department))
-    
+# def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+#     print("이름 : {}\t나이 : {}\t".format(name, age), end=" ")
+#     print(lang1, lang2, lang3, lang4, lang5)
 
-# profile("오동규", "남", "MSP")
-# profile("서동아", "여", "CC1Group")
-
-# def profile(name, age, main_lang):
-#     print("이름 : {}\t나이 : {}\t주 사용 언어: {}".format(name, age, main_lang))
-
-# profile("유재석", 20, "파이썬")
-# profile("김태호", 25, "자바")
+def profile(name, age, *language):
+    print("이름 : {}\t나이 : {}\t".format(name, age), end=" ")
+    for lang in language:
+        print(lang, end=" ")
+    print()
 
 
-### 같은 학교 같은 학년 같은 반 같은 수업.
-
-
-def profile(name, age=17, main_lang="파이썬"):
-    print("이름 : {}\t나이 : {}\t주 사용 언어: {}".format(name, age, main_lang))
-
-profile("유재석")
-profile("김태호")
+profile("유재석", 20, "Python", "java", "C", "C++", "C#", "javaScript")
+profile("김태호", 25, "kotlin", "Swift", "", "", "")
