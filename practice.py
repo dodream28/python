ee837d2022bbea035c1a8531442fad0eccb8df47
dg oh <dodream288@gmail.com>
2021-12-30 16:51:14 +0900

    Quiz#6

--- practice.py
+++ practice.py
@@ -1,17 +1,46 @@
-gun = 10
+"""
+Quiz ) 표준 체중을 구하는 프로그램을 작성하시오
 
-def checkpoint(soldiers) : # 경계근무
-    global gun # 전역 공간에 있는 gun 사용
-    gun = gun - soldiers
-    print("[함수 내] 남은 총 : {}".format(gun))
+* 표준 체중 : 각 개인의 키에 적당한 체중
 
-def checkpoint_ret(gun, soldiers):
-    gun = gun - soldiers
-    print("[함수 내] 남은 총: {}".format(gun))
-    return gun
+(성별에 따른 공식)
+남자 : 키(m) X 키(m) X 22
+여자 : 키(m) X 키(m) X 21
 
-print("전체 총 : {}".format(gun))
-# checkpoint(2) # 2명이 경계 근무 나감
+조건1 : 표준 체중은 별도의 함수 내에서 계산
+        * 함수명 : std_weight
+        * 전달값 : 키(height), 성별(gender)
 
-gun = checkpoint_ret(gun, 2)
-print("남은 총 : {}".format(gun))
+조건2 : 표준 체중은 소수점 둘째자리까지 표시
+
+(출력 예제)
+키 175cm 남자의 표준 체중은 67.38kg 입니다.
+"""
+
+# def std_weight(height, gender) :
+#     heightcm = height*0.01
+#     if gender == "여자" :
+#         weight=heightcm*heightcm*22
+#         print("키 {}cm {}의 표준 체중은 {:2.f}kg 입니다.".format(height, gender, weight))
+#     if gender == "남자" : 
+#         weight=heightcm*heightcm*21
+#         print("키 {}cm {}의 표준 체중은 {:.2f}kg 입니다.".format(height, gender, weight))
+
+# std_weight(172, "남자")
+
+def std_weight(height, gender) :
+    heightcm = height*0.01
+    if gender == "여자" :
+        weight=heightcm*heightcm*22
+        return 
+    if gender == "남자" : 
+        weight=heightcm*heightcm*21
+        return 
+height = 183
+gender = "남자"
+weight = 0
+
+print("{} {} {}".format(height, gender, weight))
+
+
+ print("키 {}cm {}의 표준 체중은 {:2.f}kg 입니다.".format(height, gender, weight))
