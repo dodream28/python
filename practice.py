b78b818ef20af2b4fed1f925f7a5e4ac32954cd4
dg oh <dodream288@gmail.com>
2021-12-30 18:56:11 +0900

    Quiz #7

--- practice.py
+++ practice.py
@@ -1,13 +1,27 @@
-# # import pickle
+"""
+Quiz) 당신의 회사에서는 매주 1회 작성해야 하는 보고서가 있습니다.
+보고서는 항상 아래와 같은 형태로 출력되어야 합니다.
 
-# # with open("profile.pickle", "rb") as profile_file:
-# #     print(pickle.load(profile_file))
+- X 주차 주간보고 -
+부서 :
+이름 : 
+업무 요약 :
 
-# with open("study.txt", "w", encoding="utf8") as study_file:
-#     study_file.write("파이썬을 열심히 공부하고 있습니다.")
+1 주차부터 50주차까지의 보고서 파일을 만드는 프로그램을 작성하시오.
 
-# with open("study.txt", "w", encoding="utf8") as study_file:
-#     study_file.write("파이썬을 열심히 공부하고 있어요")
+# 조건 : 파일명은 '1주차.txt', '2주차.txt', ... 와 같이 만듭니다.
+"""
 
-with open("study.txt", "r", encoding="utf8") as study_file:
-    print(study_file.read())
+
+for i in range(1, 51):
+    with open("{0}주차.txt".format(i), "w", encoding="utf8") as report_file:
+        report_file.write("- {0} 주차 주간보고 -\n부서 : \n이름 : \n업무 요약 : ".format(i))
+
+
+for i in range(1, 51):
+    with open(str(i) + "주차.txt", "w", encoding="utf8") as report_file:
+        report_file.wirte("- {0} 주차 주간보고 -".format(i))
+        report_file.wirte("\n부서 : ")
+        report_file.wirte("\n이름 : ")
+        report_file.wirte("\n업무 요약 : ")
+        
